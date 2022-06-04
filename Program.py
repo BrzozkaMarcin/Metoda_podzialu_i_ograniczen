167b6104f665e330ca5b005480e458cca636d118
Marcin Brzózka <mbrzozka@student.agh.edu.pl>
2022-06-04 17:33:49 +0200

    .

--- Program.py
+++ Program.py
@@ -18,19 +18,47 @@ class Little_algorithm:
     def __init__(self, matrix):
         self.matrix = matrix
 
-    def redukcja_i_odcinek(self, A: np.ndarray) -> Tuple[float, Tuple[int, int]]:
+    def reduction(self, A: np.ndarray, verbose: bool = False) -> float:
         """
-        Algorytm Little'a - redukcja macierzy
-
-        :param A: Macierz przejść do redukcji, jest redukowana in-place.
-        :type A: np.ndarray
-
-        :returns: Wartość redukcji oraz indeksy wierzchołków najlepszego odcinka do wyłączenia
-        :rtype: Tuple[float, int]
+        Redukcja macierzy in-place (krok 1)
         """
-        print("redukcja_i_odcinek - nie zaimplementowane")
-        return 0, (0, 1)
+        # sumaryczna redukcja
+        sum_red = 0
+        # odjęcie najmniejszego elementu od każdego wiersza
+        mn = np.min(A, axis=1, keepdims=True)
+        A -= mn
+        sum_red += np.sum(mn)
+        if verbose:
+            print("Redukcja w wierszach:\n{}\n".format(A))
 
-    def algorithm(self):
+        # odjęcie najmniejszego elementu od każdej kolumny
+        mn = np.min(A, axis=0, keepdims=True)
+        A -= mn
+        sum_red += np.sum(mn)
+        if verbose:
+            print("Redukcja w kolumnach:\n{}\n".format(A))
+            print("Sumaryczna redukcja: {}\n".format(sum_red))
+        return sum_red
 
-        matrix = self.redukcja_i_odcinek()
+    def optimal_edge(self, A: np.ndarray) -> Tuple[int, int]:
+        """
+        Wyszukiwanie odcinka o max koszcie wyłączenia (krok 2)
+        """
+        # === wyszukanie odcinka o najmniejszym koszcie ===
+        A = A.astype('float')
+        # zera w macierzy
+        zera = np.where(A == 0)
+        # suma maksymalna
+        suma_max = -np.inf
+        # najlepszy odcinek
+        odcinek: Tuple[int, int] = (-1, -1)
+        for i in zera[0]:
+            y = zera[0][i]
+            x = zera[1][i]
+            A[y, x] = np.inf  # tymczasowe ustawienie na inf
+            suma = np.min(A[y, :]) + np.min(A[:, x])
+            A[y, x] = 0
+            if suma > suma_max:
+                suma_max = suma
+                odcinek = (y, x)
+        return odcinek
